10901f821c9613bdd231ff27fac9aba0592ac407
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-05-24 11:56:14 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,19 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
-
+    treasure = record[0]
+    coordenada = record[1]
+    return coordenada
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
-
-
+    new_coordinate=(coordinate[0], coordinate[1])
+    return new_coordinate
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    tesoro= azara_record[0]
+    coordenada= azara_record[1]
+    new_coordinate=(coordenada[0], coordenada[1])
+    ubicacion= rui_record[0]
+    coordenadaR= rui_record[1]
+    cuadrante= rui_record[2]
+    if new_coordinate == coordenadaR :
+        ubi_final = (tesoro, coordenada, ubicacion, coordenadaR, cuadrante)
+        return ubi_final
+    else:
+        return "not a match"
